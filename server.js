49e9d4a5ccb1bfb2819b0d74d0ba8df2303faab7
blockSuho <akakak695@naver.com>
2025-04-11 13:50:09 +0900

    [feat] susu 파일 수정

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
+// require('./models/configs');
 const express = require('express')
 const path = require('path');
 const app = express();
 const categoryRouter = require('./routers/categories/all_category.routers');
 const manageClubRouter = require('./routers/club/manage_club.routers');
-
+const {mypageRouter, mainRouter} = require('./routers')
+const cookieParser = require('cookie-parser')
 
 app.set('view engine', 'ejs');
-
+app.use(express.json());
 app.use(express.urlencoded({extended : false}))
 app.use('/public', express.static(path.join(__dirname, "public")))
+app.use(cookieParser())
 
 app.use('/categories', categoryRouter);
 app.use('/clubs', manageClubRouter);
 
-app.get('/' , (req, res) => {
-    res.render('main/main')
-})
+
+
+app.use(mainRouter)
+
 
 
 app.listen(3000,() => {
