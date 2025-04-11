0e372c59cf44f6626d53826effa754826ca79f4f
zzeen2 <jking120393@gmail.com>
2025-04-11 14:08:18 +0900

    [merge] je, jh 커밋

--- routers/club/detail_club.routers.js
+++ routers/club/detail_club.routers.js
@@ -3,3 +3,9 @@ const axios = require('axios');
 const categoryController = require("../../controllers/club/add_club.controllers");
 const jwt = require("jsonwebtoken")
 const {upload} = require("../../lib/multer")
+
+//-------------------- 프론트
+router.get("/", (req,res)=> {
+    res.render("/")
+})
+//-------------------- 백엔드
